f59cd77ca0de3eb833829f6178bdeee047b419b1
Euclides Neto <45155322+euclidescarlos@users.noreply.github.com>
2025-03-30 18:51:39 -0300

    Create README.mk

---
+++ README.mk
@@ -0,0 +1,13 @@
+# Contador Android 🏧
+
+Aplicativo mobile Contador desenvolvido em Kotlin no Android Studio para a disciplina de Desenvolvimento para Dispositivos Móveis do 3º período de Análise e Desenvolvimento de Sistemas.
+
+## 👥 Equipe ADS - 3º Período
+
+- **Angelo Santos** - Matrícula: 01707596
+- **Eduardo Henrique** - Matrícula: 01706530
+- **Euclides Neto** - Matrícula: 01696172
+- **Gabriel Teixeira** - Matrícula: 01413025
+- **Ingrid Larissa** - Matrícula: 01552364
+- **Klebson Apolinário** - Matrícula: 01704173
+- **Samara Jovino** - Matrícula: 01711332
